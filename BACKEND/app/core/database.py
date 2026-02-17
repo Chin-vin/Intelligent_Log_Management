8f253f8c2a648dc3e6696477ea3e7fbdead94b29
Chin-vin <chincholivinitha195@gmail.com>
2026-02-17 12:44:21 +0530

    Testing

--- BACKEND/app/core/database.py
+++ BACKEND/app/core/database.py
@@ -46,6 +46,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL:",DATABASE_URL)
 
 engine = create_engine(
     DATABASE_URL,

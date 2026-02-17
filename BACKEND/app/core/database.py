f99d39e5ef70ff5b6aebab74d6a28384093eaae1
Chin-vin <chincholivinitha195@gmail.com>
2026-02-17 12:59:57 +0530

    Testing

--- BACKEND/app/core/database.py
+++ BACKEND/app/core/database.py
@@ -1,13 +1,34 @@
+# # # from sqlalchemy import create_engine
+# # # from sqlalchemy.orm import sessionmaker, declarative_base
+
+# # # from sqlalchemy.orm import Session
+
+# # # DATABASE_URL = "postgresql://postgres:techv1%403@localhost:5432/Intelligent_Log_File_Management"
+
+# # # engine = create_engine(DATABASE_URL)
+# # # SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+# # # Base = declarative_base()
+
+
+# # # def get_db():
+# # #     db = SessionLocal()
+# # #     try:
+# # #         yield db
+# # #     finally:
+# # #         db.close()
+# # import os
 # # from sqlalchemy import create_engine
 # # from sqlalchemy.orm import sessionmaker, declarative_base
 
-# # from sqlalchemy.orm import Session
+# # DATABASE_URL = os.getenv("DATABASE_URL")
 
-# # DATABASE_URL = "postgresql://postgres:techv1%403@localhost:5432/Intelligent_Log_File_Management"
+# # engine = create_engine(
+# #     DATABASE_URL,
+# #     connect_args={"sslmode": "require"}
+# # )
 
-# # engine = create_engine(DATABASE_URL)
 # # SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
 # # Base = declarative_base()
 
 
@@ -18,14 +39,17 @@
 # #     finally:
 # #         db.close()
 # import os
+# from dotenv import load_dotenv
 # from sqlalchemy import create_engine
 # from sqlalchemy.orm import sessionmaker, declarative_base
 
+# load_dotenv()
+
 # DATABASE_URL = os.getenv("DATABASE_URL")
+# print("DATABASE_URL:",DATABASE_URL)
 
 # engine = create_engine(
 #     DATABASE_URL,
-#     connect_args={"sslmode": "require"}
 # )
 
 # SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
@@ -39,18 +63,14 @@
 #     finally:
 #         db.close()
 import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-print("DATABASE_URL:",DATABASE_URL)
+print("DATABASE_URL:", DATABASE_URL)
 
-engine = create_engine(
-    DATABASE_URL,
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()

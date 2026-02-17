f475ec3d5563677322a5380e3e8bee6f01f2e0f7
Chin-vin <chincholivinitha195@gmail.com>
2026-02-17 19:22:18 +0530

    Updated codebase

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -37,38 +37,3 @@ api.interceptors.response.use(
 
 
 export default api;
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("access_token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// /* ✅ RESPONSE: Handle token expiry */
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     const status = error.response?.status;
-//     const url = error.config?.url;
-
-//     if (status === 401 && !url.includes("/auth/login")) {
-//       console.warn("Token expired or unauthorized");
-//       localStorage.clear();
-//       window.location.href = "/login";
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
